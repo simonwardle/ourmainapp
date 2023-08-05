78576b3df7300f2583ad5c75abf44ff2841ca9ab
simonwardle <simon.wardle@outlook.com>
2023-08-05 18:22:42 +0100

    completed 32 Middleware

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,15 @@ Route::post('/register', [UserController::class, "register"])->middleware('guest
 Route::post('/login', [UserController::class, "login"])->middleware('guest');
 Route::post('/logout', [UserController::class, "logout"])->middleware('mustBeLoggedIn');
 
+//Profile related routes
+Route::get('/profile/{user:username}', [UserController::class, "profile"]);
+
 //Blog post related routes
 Route::get('/create-post', [PostController::class, "showCreateForm"])->middleware('mustBeLoggedIn');
 Route::post('/create-post', [PostController::class, "storeNewPost"])->middleware('mustBeLoggedIn');
 Route::get('/post/{post}', [PostController::class, "viewSinglePost"]);
 Route::delete('/post/{post}', [PostController::class, "delete"])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, "showEditForm"])->middleware('can:update,post');
+Route::put('/post/{post}', [PostController::class, "actuallyUpdate"])->middleware('can:update,post');
 
-//Profile related routes
-Route::get('/profile/{user:username}', [UserController::class, "profile"]);
 
